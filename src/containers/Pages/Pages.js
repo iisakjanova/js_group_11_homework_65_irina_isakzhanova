b9c3a70d9519c1d3fda4c329e5b3a8cba518a18e
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-08-31 15:38:34 +0600

    Realized pages content show

--- src/containers/Pages/Pages.js
+++ src/containers/Pages/Pages.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axiosApi from "../../axiosApi";
 
-const Pages = () => {
+import './Pages.css';
+
+const Pages = ({match}) => {
+    const [page, setPage] = useState('');
+
+    useEffect(() => {
+        (async () => {
+            const page = await getPage();
+            setPage(page);
+        })();
+    }, [match.params.id]);
+
+    const getPage = async () => {
+        const response = await axiosApi('/pages/' + match.params.id + '.json');
+        return response.data;
+    };
+
     return (
-        <div>
-            Page
+        <div className="Page">
+            <h2 className="Title">{page.title}</h2>
+            <p className="Content">{page.content}</p>
         </div>
     );
 };
